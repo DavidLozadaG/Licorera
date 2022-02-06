a9aaef7357510dcac76852c54db6bce4031539f5
EdgarDavidLozada <edgarlozada662@gmail.com>
2022-02-06 16:15:38 -0500

    agregado de base de datos

---
+++ BASE DE DATOS/Modelo Relacional/MR_Licorera.sql
@@ -0,0 +1,306 @@
+DROP DATABASE licorera;
+CREATE DATABASE licorera;
+USE licorera;
+CREATE TABLE IF NOT EXISTS roles(
+    cod_rol INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomb_rol VARCHAR(45) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS usuarios(
+    cedula VARCHAR(20) NOT NULL PRIMARY KEY,
+    nombres VARCHAR(45) NOT NULL,
+    apellidos VARCHAR(45) NOT NULL,
+    fecha_nac DATE NOT NULL,
+    edad INT,
+    telefono VARCHAR(10) NOT NULL,
+    email VARCHAR(256) NOT NULL UNIQUE,
+    clave VARCHAR(256) NOT NULL,
+    cod_rol INT NOT NULL,
+	CHECK(fecha_nac < SYSDATE()),
+    CHECK(edad>=18),
+    CHECK(length(telefono)=10),
+    CONSTRAINT fk_cod_rol FOREIGN KEY (cod_rol) REFERENCES roles(cod_rol) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS empresas(
+    nit_empresa VARCHAR(45) NOT NULL PRIMARY KEY,
+    nomb_empresa VARCHAR(45) NOT NULL UNIQUE,
+    direccion VARCHAR(45) NOT NULL,
+    telefono VARCHAR(13) NOT NULL,
+	CHECK(length(telefono)>=7)
+);
+
+CREATE TABLE IF NOT EXISTS categorias(
+    cod_cat INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomb_cat VARCHAR(45) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS licores(
+    cod_licor INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomb_licor VARCHAR(45) NOT NULL UNIQUE,
+    foto VARCHAR(100),
+    grado_alcoh DOUBLE NOT NULL,
+    descripcion VARCHAR(45) NOT NULL,
+    cantidad INT NOT NULL,
+    precio INT NOT NULL,
+    cod_cat INT NOT NULL,
+	CHECK(cantidad>=0),
+    CHECK(precio>0),
+    CONSTRAINT fk_cod_cat FOREIGN KEY (cod_cat) REFERENCES categorias(cod_cat) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS proveedores(
+    cod_licor INT NOT NULL,
+    nit_empresa VARCHAR(45) NOT NULL,
+    PRIMARY KEY(cod_licor,nit_empresa),
+    CONSTRAINT fk_cod_licor FOREIGN KEY (cod_licor) REFERENCES licores(cod_licor) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_nit_empresa FOREIGN KEY (nit_empresa) REFERENCES empresas(nit_empresa) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS metodos_pagos(
+    cod_metodo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomb_metodo VARCHAR(45) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS estados_pedido(
+    cod_estado INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomb_estado VARCHAR(45) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS pedidos(
+    cod_pedido INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fecha_pedido DATETIME NOT NULL,
+    cedula VARCHAR(20) NOT NULL,
+    cod_metodo INT NOT NULL,
+	direccion_envio VARCHAR(45) NOT NULL,
+    cod_estado INT NOT NULL,
+    observacion VARCHAR(45),
+    CONSTRAINT fk_cedula FOREIGN KEY (cedula) REFERENCES usuarios(cedula) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_cod_metodo FOREIGN KEY (cod_metodo) REFERENCES metodos_pagos(cod_metodo) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_cod_estado FOREIGN KEY (cod_estado) REFERENCES estados_pedido(cod_estado) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS detalles(
+    cod_pedido INT NOT NULL,
+    cod_licor INT NOT NULL,
+    PRIMARY KEY (cod_pedido,cod_licor),
+    cantidad INT NOT NULL,
+    CHECK(cantidad>0),
+    CONSTRAINT fk_cod_pedido FOREIGN KEY (cod_pedido) REFERENCES pedidos(cod_pedido) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_cod_licor_detalles FOREIGN KEY (cod_licor) REFERENCES licores(cod_licor) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS historial_facturas(
+    cod_pedido INT NOT NULL PRIMARY KEY,
+    fecha_pedido DATETIME NOT NULL,
+    cedula VARCHAR(20) NOT NULL,
+    nombre_completo VARCHAR(90) NOT NULL,
+    direccion_envio VARCHAR(45) NOT NULL,
+    cantidad_total INT,
+    pago_total INT,
+    nomb_metodo VARCHAR(45) NOT NULL,
+    nomb_estado VARCHAR(45) NOT NULL,
+    observacion VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS detalles_pedido(
+    cod_licor INT NOT NULL,
+    nomb_licor VARCHAR(45) NOT NULL,
+    descripcion VARCHAR(45) NOT NULL,
+    cantidad INT NOT NULL,
+    precio_und INT NOT NULL,
+    sub_total INT NOT NULL,
+    cod_pedido INT NOT NULL,
+    PRIMARY KEY (cod_pedido,cod_licor),
+    CONSTRAINT fk_historial_cod_pedido FOREIGN KEY (cod_pedido) REFERENCES historial_facturas(cod_pedido) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+#DISPARADOR PARA AGREGAR LA OBSERVACION DE UN PEDIDO CUANDO ESTE ES INSERTADO
+DELIMITER //
+DROP TRIGGER IF EXISTS insertar_observacion_pedido//
+CREATE TRIGGER insertar_observacion_pedido
+BEFORE INSERT
+ON pedidos FOR EACH ROW 
+BEGIN
+	DECLARE estado_entrega VARCHAR(45);
+    SET estado_entrega = (SELECT nomb_estado FROM estados_pedido WHERE cod_estado = NEW.cod_estado);
+	IF estado_entrega = 'PENDIENTE' THEN
+		SET NEW.observacion = 'PEDIDO EN PROCESO DE REVISION';
+    END IF;
+	IF estado_entrega = 'ENVIADO' THEN
+		SET NEW.observacion = 'PEDIDO EN CAMINO A DIRECION DE ENTREGA' ;
+    END IF;
+    IF estado_entrega = 'ENTREGADO' THEN
+		SET NEW.observacion = 'PEDIDO ENVIADO Y ENTREGADO';
+    END IF;
+    IF estado_entrega = 'CANCELADO' THEN
+		SET NEW.observacion = NEW.observacion;
+    END IF;
+END;//
+DELIMITER ;
+
+#DISPARADOR PARA INSERTAR LOS DATOS A UN HISTORIAL DE PEDIDOS
+DELIMITER //
+DROP TRIGGER IF EXISTS insertar_datos_historial//
+CREATE TRIGGER insertar_datos_historial
+AFTER INSERT 
+ON pedidos FOR EACH ROW
+BEGIN
+	DECLARE nombre_completo VARCHAR(45);
+	DECLARE metodo VARCHAR(45);
+	DECLARE estado_entrega VARCHAR(45);
+	SET nombre_completo = (SELECT concat(nombres," ",apellidos) FROM usuarios WHERE cedula = NEW.cedula);
+	SET metodo = (SELECT nomb_metodo FROM metodos_pagos WHERE cod_metodo = NEW.cod_metodo);
+	SET estado_entrega = (SELECT nomb_estado FROM estados_pedido WHERE cod_estado = NEW.cod_estado);
+	INSERT INTO historial_facturas VALUES(NEW.cod_pedido, NEW.fecha_pedido, NEW.cedula, nombre_completo, NEW.direccion_envio, NULL, NULL, metodo, estado_entrega,NEW.observacion);
+END; //
+DELIMITER ;
+
+#DISPARADOR PARA ACTUALIZAR EL ESTADO DEL PEDIDO
+DELIMITER //
+DROP TRIGGER IF EXISTS actualizar_estado_pedido//
+CREATE TRIGGER actualizar_estado_pedido
+BEFORE UPDATE
+ON pedidos FOR EACH ROW
+BEGIN
+	DECLARE estado_entrega VARCHAR(45);
+	SET estado_entrega = (SELECT nomb_estado FROM estados_pedido WHERE cod_estado = NEW.cod_estado);
+    IF estado_entrega = 'PENDIENTE' THEN
+		SET NEW.observacion = 'PEDIDO EN PROCESO DE REVISION';
+        CALL conteo_licor(NEW.cod_pedido,estado_entrega);
+    	UPDATE historial_facturas SET nomb_estado = estado_entrega, observacion = NEW.observacion WHERE cod_pedido = NEW.cod_pedido;
+    END IF;
+	IF estado_entrega = 'ENVIADO' THEN
+		SET NEW.observacion = 'PEDIDO EN CAMINO A DIRECION DE ENTREGA';
+		UPDATE historial_facturas SET nomb_estado = estado_entrega, observacion = NEW.observacion WHERE cod_pedido = NEW.cod_pedido;
+    END IF;
+    IF estado_entrega = 'ENTREGADO' THEN
+		SET NEW.observacion = 'PEDIDO ENVIADO Y ENTREGADO';
+		UPDATE historial_facturas SET nomb_estado = estado_entrega, observacion = NEW.observacion WHERE cod_pedido = NEW.cod_pedido;
+    END IF;
+    IF estado_entrega = 'CANCELADO' THEN
+		SET NEW.observacion = 'PEDIDO CANCELADO POR X MOTIVO';
+        CALL conteo_licor(NEW.cod_pedido,estado_entrega);
+		UPDATE historial_facturas SET nomb_estado = estado_entrega, observacion = NEW.observacion WHERE cod_pedido = NEW.cod_pedido;
+    END IF;
+END; //
+DELIMITER ;
+
+#DISPARADOR PARA INGRESAR LOS DETALLES DEL PEDIDO PARA CADA HISTORIAL DE FACTURA
+DELIMITER //
+DROP TRIGGER IF EXISTS insertar_detalles_pedidos//
+CREATE TRIGGER insertar_detalles_pedidos
+AFTER INSERT  
+ON detalles FOR EACH ROW
+BEGIN
+	DECLARE licor VARCHAR(45);
+	DECLARE descrip VARCHAR(45);
+	DECLARE precio_und INT;
+	DECLARE sub_total INT;
+    DECLARE cantidad_total INT;
+	DECLARE pago_total INT;
+	SET licor = (SELECT nomb_licor FROM licores WHERE cod_licor = NEW.cod_licor);
+	SET descrip = (SELECT descripcion FROM licores WHERE cod_licor = NEW.cod_licor);
+	SET precio_und = (SELECT precio FROM licores WHERE cod_licor = NEW.cod_licor);
+	SET sub_total = (NEW.cantidad*precio_und);
+    SET cantidad_total = (SELECT SUM(cantidad) FROM detalles WHERE cod_pedido = NEW.cod_pedido GROUP BY cod_pedido);
+	SET pago_total = (SELECT SUM(l.precio*d.cantidad) FROM detalles AS d INNER JOIN licores as l ON d.cod_licor=l.cod_licor WHERE d.cod_pedido = NEW.cod_pedido GROUP BY d.cod_pedido);
+	INSERT INTO detalles_pedido VALUES(NEW.cod_licor, licor, descrip, NEW.cantidad, precio_und, sub_total, NEW.cod_pedido);
+    UPDATE historial_facturas SET cantidad_total = cantidad_total ,pago_total = pago_total WHERE cod_pedido=NEW.cod_pedido;
+END; //
+DELIMITER ;
+
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+#FUNCION PARA CALCULAR LA EDAD ACTUAL DE UNA PERSONA SEGUN SU FECHA DE NACIMIENTO
+DELIMITER //
+DROP FUNCTION IF EXISTS calcular_edad//
+CREATE FUNCTION calcular_edad(fecha_nac DATE)
+RETURNS INT
+BEGIN
+	DECLARE edad INT;
+    SET edad = TIMESTAMPDIFF(YEAR,fecha_nac,CURDATE());
+	RETURN edad;
+END; //
+DELIMITER ;
+
+#DISPARADOR PARA AGREGAR LA EDAD EN LA TABLA PERSONAS
+DELIMITER //
+DROP TRIGGER IF EXISTS adicionar_edad//
+CREATE TRIGGER adicionar_edad
+BEFORE INSERT 
+ON usuarios FOR EACH ROW
+BEGIN
+	SET NEW.edad = (SELECT calcular_edad(NEW.fecha_nac));
+END; //
+DELIMITER ;
+
+#DISPARADOR PARA AGREGAR LA FECHA EN LA QUE SE HACE UN PEDIDO
+DELIMITER //
+DROP TRIGGER IF EXISTS adicionar_fecha_pedido//
+CREATE TRIGGER adicionar_fecha_pedido
+BEFORE INSERT 
+ON pedidos FOR EACH ROW
+BEGIN
+	IF NEW.fecha_pedido = '0000-00-00' THEN
+		SET NEW.fecha_pedido = SYSDATE();
+	END IF;
+END; //
+DELIMITER ;
+
+#FUNCION PARA CALCULAR LA CANTIDAD TOTAL DE UN LICOR CUANDO ESTE SEA COMPRADO O SU COMPRA SEA CANCELADA)
+DELIMITER //
+DROP FUNCTION IF EXISTS calcular_licor//
+CREATE FUNCTION calcular_licor(cod_licor INT, cantidad INT)
+RETURNS INT
+BEGIN
+    DECLARE cantidad_actual INT;
+	DECLARE cantidad_licor INT; 
+    SET cantidad_actual = (SELECT l.cantidad FROM licores AS l WHERE l.cod_licor = cod_licor);
+    SET cantidad_licor = ( cantidad_actual + cantidad);
+	RETURN cantidad_licor;
+END; //
+DELIMITER ;
+
+#DISPARADOR PARA RESTAR LA CANTIDAD DE UN LICOR LUEGO DE SER COMPRADO
+DELIMITER //
+DROP TRIGGER IF EXISTS eliminar_cantidad_licor//
+CREATE TRIGGER eliminar_cantidad_licor
+BEFORE INSERT 
+ON detalles FOR EACH ROW
+BEGIN
+	DECLARE estado VARCHAR(45);
+    SET estado = (SELECT (SELECT nomb_estado FROM estados_pedido WHERE cod_estado = p.cod_estado) AS estado FROM pedidos AS p WHERE p.cod_pedido = NEW.cod_pedido);
+	IF estado != 'CANCELADO' THEN
+		UPDATE licores SET cantidad = (SELECT calcular_licor(NEW.cod_licor,-NEW.cantidad)) WHERE cod_licor = NEW.cod_licor;
+    END IF;
+END; //
+DELIMITER ;
+
+#PROCEDIMIENTO PARA TENER CONTROL DEL CONTEO DE LOS LICORES SEGUN LA ACTUALIZACION DEL ESTADO DEL PEDIDO
+DELIMITER //
+DROP PROCEDURE IF EXISTS conteo_licor//
+CREATE PROCEDURE conteo_licor(pedido INT,estado VARCHAR(45))
+	BEGIN 
+		DECLARE listo INTEGER DEFAULT 0;
+		DECLARE licor INT;
+		DECLARE cantidad_licor INT;
+
+		DECLARE c1 CURSOR FOR SELECT cod_licor, cantidad FROM detalles WHERE cod_pedido = pedido;
+		DECLARE CONTINUE HANDLER FOR NOT FOUND SET listo = 1;
+		OPEN c1;
+
+		l1:loop
+		FETCH c1 into licor, cantidad_licor;
+			IF listo = 1 THEN LEAVE l1;
+			END IF;
+            IF estado ='PENDIENTE' THEN
+			UPDATE licores SET cantidad = (SELECT calcular_licor(licor, -cantidad_licor)) WHERE cod_licor = licor;
+            END IF;
+            IF estado ='CANCELADO' THEN
+			UPDATE licores SET cantidad = (SELECT calcular_licor(licor, cantidad_licor)) WHERE cod_licor = licor;
+            END IF;
+		END LOOP l1;
+		CLOSE c1;
+	END; //
+DELIMITER ;
---
+++ BASE DE DATOS/Modelo Relacional/licoreraDatos.sql
@@ -0,0 +1,93 @@
+#roles
+INSERT INTO roles (nomb_rol) values('ADMINISTRADOR');
+INSERT INTO roles (nomb_rol) values('CLIENTE');
+INSERT INTO roles (nomb_rol) values('REPARTIDOR');
+
+#estados_pedidos
+INSERT INTO estados_pedido (nomb_estado) values('PENDIENTE');
+INSERT INTO estados_pedido (nomb_estado) values('ENVIADO');
+INSERT INTO estados_pedido (nomb_estado) values('ENTREGADO');
+INSERT INTO estados_pedido (nomb_estado) values('CANCELADO');
+
+#personas
+INSERT INTO usuarios values('1566856457','EDGAR DAVID','LOZADA GONZALEZ','2000-07-24',NULL,'3228190672','edgarlozada663@gmail.com','564546465',1);
+INSERT INTO usuarios values('1226857029','ANDREA CAMILA','FIGUEROA PEREZ','1998-10-13',NULL,'3108560531','andrea@gmail.com','54665456',2);
+
+#empresas
+INSERT INTO empresas values('DA48756Q9886','BAVARIA','CL 19 A 34-474 DC.BGTA','0180010');
+INSERT INTO empresas values('DS48/WDA4566','LICO AS','CL 45 A 34-474 DC.BGTA','1796002');
+
+#categorias
+INSERT INTO categorias values(NULL,'AGUARDIENTE');
+INSERT INTO categorias values(NULL,'CERVEZA');
+INSERT INTO categorias values(NULL,'RON');
+INSERT INTO categorias values(NULL,'WHISKEY');
+INSERT INTO categorias values(NULL,'TEQUILA');
+INSERT INTO categorias values(NULL,'VODKA');
+INSERT INTO categorias values(NULL,'VINO');
+INSERT INTO categorias values(NULL,'GINEBRA');
+
+#licores
+INSERT INTO licores values(NULL,'AGUILA','SixAguila330ml.jpg',4.5,'CERVEZA AGUILA LATA SIX PACK-330ML',100,12050,2);
+INSERT INTO licores values(NULL,'POKER','SixPoker330ml.jpg',4.5,'CERVEZA POKER LATA SIX PACK-330ML',100,15900,2);
+INSERT INTO licores values(NULL,'BUDWEISER','SixBudweiser355ml.jpg',5.0,'CERVEZA BUDWEISER LATA SIX PACK-255ML',100,12700,2);
+INSERT INTO licores values(NULL,'CORONA','SixCorona355ml.jpg',4.5,'CERVEZA CORONA EXTRA SIX PACK-355ML',100,23900,2);
+INSERT INTO licores values(NULL,'REDDS','SixRedds269ml.jpg',4.5,'CERVEZA REDDS LATA SIX PACK-269ML',100,16000,2);
+INSERT INTO licores values(NULL,'SMIRNOFF LULO','SmirnoffLulo375ml.jpg',25,'LICOR DE VODKCA SMIRNOFF SABOR LULO-375ML',50,1710,6);
+INSERT INTO licores values(NULL,'JOHNNIE WALKER RED LABEL','JohnnieWalkerRedLabel1000ml.jpg',40,'WHISKEY WALKER RED LABEL-1000ML',50,53900,4);
+INSERT INTO licores values(NULL,'GORDONS PREMIUM PINK','GordonsPremiumPink700ml.jpg',37.5,'GINEBRA PREMIUM PINK-700ML',50,68900,8);
+INSERT INTO licores values(NULL,'ZACAPA AMBAR','ZapacaAmbarCentenario750ml.jpg',40,'RON CENTENARIO-750ML',50,94900,3);
+
+#abastece
+INSERT INTO proveedores values(1,'DA48756Q9886');
+INSERT INTO proveedores values(2,'DA48756Q9886');
+INSERT INTO proveedores values(3,'DA48756Q9886');
+INSERT INTO proveedores values(4,'DA48756Q9886');
+INSERT INTO proveedores values(5,'DA48756Q9886');
+INSERT INTO proveedores values(6,'DS48/WDA4566');
+INSERT INTO proveedores values(7,'DS48/WDA4566');
+INSERT INTO proveedores values(8,'DS48/WDA4566');
+INSERT INTO proveedores values(9,'DS48/WDA4566');
+
+#metodo de pago
+INSERT INTO metodos_pagos VALUES(NULL,'CONTRA-ENTREGA');
+INSERT INTO metodos_pagos VALUES(NULL,'TRANSFERENCIA BANCARIA');
+
+#pedidos
+INSERT INTO pedidos VALUES(NULL,'2021-06-12','1006856027',1,'CL 19 A SUR # 38-29',1,NULL);
+INSERT INTO pedidos VALUES(NULL,'2021-08-13','1006856027',2,'CL 17 B SUR # 18-49',2,NULL);
+INSERT INTO pedidos VALUES(NULL,'2021-08-13','1006856028',1,'CL 24 C NOR # 01-12',3,NULL);
+INSERT INTO pedidos VALUES(NULL,'2021-08-14','1006856028',2,'CL 65 U NOR # 35-02',4,'EL CLIENTE NO REALIZO EL PAGO');
+INSERT INTO pedidos VALUES(NULL,'0000-00-00','1006856027',1,'CL 86 U SUE # 25-36',2,NULL);
+
+#detalles
+INSERT INTO detalles VALUES(1,3,4);
+INSERT INTO detalles VALUES(1,4,1);
+INSERT INTO detalles VALUES(1,7,1);
+INSERT INTO detalles VALUES(2,2,7);
+INSERT INTO detalles VALUES(2,1,3);
+INSERT INTO detalles VALUES(2,6,1);
+INSERT INTO detalles VALUES(3,3,4);
+INSERT INTO detalles VALUES(3,4,8);
+INSERT INTO detalles VALUES(3,5,6);
+INSERT INTO detalles VALUES(3,8,9);
+INSERT INTO detalles VALUES(4,1,1);
+INSERT INTO detalles VALUES(4,9,2);
+INSERT INTO detalles VALUES(4,7,3);
+INSERT INTO detalles VALUES(4,2,4);
+INSERT INTO detalles VALUES(5,7,5);
+INSERT INTO detalles VALUES(5,1,10);
+
+#CONSULTA DE DETALLES DE UN PEDIDO
+SELECT l.nomb_licor AS LICOR, l.descripcion AS DESCRIPCION, d.cantidad AS CANTIDAD, l.precio AS PRECIO_UND, (l.precio*d.cantidad) AS SUBTOTAL
+FROM pedidos AS p 
+INNER JOIN detalles AS d ON p.cod_pedido=d.cod_pedido
+INNER JOIN licores AS l ON d.cod_licor=l.cod_licor
+WHERE p.cod_pedido = 1;
+
+SELECT * FROM licores;
+SELECT * FROM pedidos;
+SELECT * FROM usuarios;
+SELECT * FROM historial_facturas;
+
+UPDATE pedidos SET cod_estado = 1 WHERE cod_pedido =1;
